eadaff623c6135a059ec9d9a5383bb2e2873dec2
Andrey Tipapro <tipapro@mail.ru>
2018-07-13 01:49:25 +0300

    Добавьте файлы проекта.

---
+++ ANBOTIP-Rainbow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ANBOTIP_Rainbow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
